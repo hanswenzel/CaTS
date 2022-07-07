c8d9c941ff4d6e6c0e13537c6ee6d1a98457ca1f
Hans Wenzel <wenzel@fnal.gov>
2022-07-07 14:34:32 -0500

    Add files via upload
    add script for Ubuntu system to make sure all necessary system tool and development libraries are present.

---
+++ scripts/checkpr.sh
@@ -0,0 +1,53 @@
+sudo apt-get --assume-yes install build-essential
+sudo apt-get --assume-yes install git
+sudo apt-get --assume-yes install cmake
+sudo apt-get --assume-yes install ninja-build
+
+sudo apt-get --assume-yes install clang
+sudo apt-get --assume-yes install llvm
+sudo apt-get --assume-yes install clang-format
+sudo apt-get --assume-yes install clang-tidy
+sudo apt-get --assume-yes install iwyu
+
+sudo apt-get --assume-yes install libexpat1-dev
+sudo apt-get --assume-yes install zlib1g-dev
+sudo apt-get --assume-yes install libxerces-c-dev
+sudo apt-get --assume-yes install qtbase5-dev
+sudo apt-get --assume-yes install libxmu-dev
+
+sudo apt-get --assume-yes install libxpm-dev
+sudo apt-get --assume-yes install libxft-dev
+
+sudo apt-get --assume-yes install curl
+sudo apt-get --assume-yes install python-is-python3
+sudo apt-get --assume-yes install libssl-dev
+sudo apt-get --assume-yes install libxrandr-dev
+sudo apt-get --assume-yes install libglfw3-dev
+sudo apt-get --assume-yes install libglew-dev
+sudo apt-get --assume-yes install libglm-dev
+sudo apt-get --assume-yes install libxcursor-dev
+sudo apt-get --assume-yes install libxi-dev
+sudo apt-get --assume-yes install libxinerama-dev
+sudo apt-get --assume-yes install libboost-all-dev
+#
+sudo apt-get --assume-yes install synaptic
+sudo apt-get --assume-yes install engauge-digitizer
+sudo apt-get --assume-yes install plocate
+sudo apt-get --assume-yes install dkms
+sudo apt-get --assume-yes install a2ps
+sudo apt-get --assume-yes install rdesktop
+sudo apt-get --assume-yes install krb5-user
+#   sudo dpkg -i code_1.67.1-1651841865_amd64.deb
+sudo apt-get --assume-yes install net-tools
+sudo apt-get --assume-yes install freeglut3-dev
+sudo apt-get --assume-yes install libopenmpi-dev
+sudo apt-get update
+sudo apt-get -y install nvidia-cuda-toolkit
+#git clone https://github.com/NVIDIA/cuda-samples.git
+#cd cuda-samples
+#make -j 8
+#sudo ln -s /home/wenzel/cuda-samples /usr/local/cuda/samples
+#zoom
+sudo apt-get --assume-yes install libgl1-mesa-glx
+sudo apt-get --assume-yes install libxcb-xtest0
+sudo apt-get --assume-yes install libxcb-xtest0-dev
